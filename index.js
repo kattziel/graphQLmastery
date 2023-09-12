6276d4371273cfb736f4651fafa682350621719b
Katarzyna Zielinska <jekaterina.zielinska@gmail.com>
2023-09-12 14:11:32 +0200

    add greeting and interesting urls data

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
-const { ApolloServer } = require("apollo-server");
-const typeDefs = `
-type Query {
-    greeting: String}
+const { ApolloServer, gql } = require("apollo-server");
+const typeDefs = gql`
+  type Query {
+    greeting: String
+    interestingUrls: [String]
+  }
 `;
-const server = new ApolloServer({ typeDefs });
+
+const data = {
+  greeting: "Hello, this is primary greeting vol. 2!",
+  interestingUrls: ["https://www.google.com"]
+};
+const server = new ApolloServer({ typeDefs, rootValue: data });
 server.listen({ port: 4000 }).then((result) => console.log(result.url));
