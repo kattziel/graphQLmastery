ec3209c57886eb240d97af9b8386a943ae7ad379
Katarzyna Zielinska <jekaterina.zielinska@gmail.com>
2023-09-13 17:46:30 +0200

    amend landing page options - trial

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 const { ApolloServer, gql } = require("apollo-server");
+const {
+    ApolloServerPluginLandingPageLocalDefault,
+    ApolloServerPluginLandingPageProductionDefault,
+  } = require("apollo-server-core");
+
 const PORT = process.env.PORT || 4000;
 const typeDefs = gql`
   type Query {
@@ -17,6 +22,11 @@ const server = new ApolloServer({
   playground: true,
   introspection: true,
   cache: "bounded",
+  plugins: [
+    process.env.NODE_ENV === 'production'
+      ? ApolloServerPluginLandingPageProductionDefault({ embed: true, graphRef: "my-graph-id@my-graph-variant" })
+      : ApolloServerPluginLandingPageLocalDefault({ embed: false }),
+  ],
 });
 
 server.listen({ port: PORT }).then((result) => console.log(result.url));
