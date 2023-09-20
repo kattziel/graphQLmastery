ef70b7ac3f3341af8325b3a476359d963f1bab0e
Katarzyna Zielinska <jekaterina.zielinska@gmail.com>
2023-09-20 18:45:05 +0200

    add dynamic rootValue

--- index.js
+++ index.js
@@ -1,24 +1,29 @@
 const { ApolloServer, gql } = require("apollo-server");
 const {
-    ApolloServerPluginLandingPageLocalDefault,
-    ApolloServerPluginLandingPageProductionDefault,
-  } = require("apollo-server-core");
+  ApolloServerPluginLandingPageLocalDefault,
+  ApolloServerPluginLandingPageProductionDefault,
+} = require("apollo-server-core");
 
 const PORT = process.env.PORT || 4000;
 const typeDefs = gql`
   type Query {
     greeting: String
     interestingUrls: [String]
+    randomDiceThrow: Int
   }
 `;
-
-const data = {
-  greeting: "Hello, this is primary greeting vol. 2!",
-  interestingUrls: ["https://www.google.com", "https://www.github.com"],
-};
+function rootValue() {
+  const getRandomDiceThrow = (sides) => Math.ceil(Math.random() * sides);
+  const data = {
+    greeting: "Hello, this is primary greeting vol. 2!",
+    interestingUrls: ["https://www.google.com", "https://www.github.com"],
+    randomDiceThrow: getRandomDiceThrow(6)
+  };
+  return data;
+}
 const server = new ApolloServer({
   typeDefs,
-  rootValue: data,
+  rootValue,
   playground: true,
   introspection: true,
   cache: "bounded",
